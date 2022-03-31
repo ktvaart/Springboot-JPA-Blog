f95eecba595f93ad3bf21446005478058392b514
ktkim <vaart5576@gmail.com>
2022-04-01 00:30:05 +0900

    환경셋팅 완료

---
+++ src/test/java/com/ktkim/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktkim.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
